4506753d11dfe988ed5cdde7473b99c0713212ac
Alejandro Verdugo <72816617+AlejandroVerdugo@users.noreply.github.com>
2021-03-14 12:15:26 -0600

    Update dibujo.js

--- dibujo.js
+++ dibujo.js
@@ -6,29 +6,9 @@ var y = 552;
 lienzo.fillStyle = "black";
 lienzo.fillRect(0, 0, x, y);
 
-lienzo.beginPath();
 lienzo.strokeStyle = "white";
-marco();
+lienzo.lineWidth = 2;
+lienzo.strokeRect(18, 18, 358, 516);
+lienzo.arc(200, 190, 150, 0, 2 * Math.PI);
 lienzo.stroke();
 lienzo.closePath();
-function marco() {
-  for (let i = 0; i < 10; i++) {
-    lienzo.moveTo(18, 18);
-    lienzo.lineTo(18, y - 18);
-    lienzo.lineTo(x - 18, y - 18);
-    lienzo.lineTo(x - 18, 18);
-    lienzo.lineTo(18, 18);
-  }
-}
-
-lienzo.beginPath();
-lienzo.strokeStyle = "white";
-circulo();
-lienzo.stroke();
-lienzo.closePath();
-
-function circulo() {
-  for (let i = 0; i < 10; i++) {
-    lienzo.arc(200, 190, 150, 0, 2 * Math.PI);
-  }
-}
